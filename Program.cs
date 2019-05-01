55c8b20b899b2b07e552c25741f88d74322f932c
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-05-01 11:25:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,40 +10,27 @@ namespace Icarus
     {
         static void Main(string[] args)
         {
-
             List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-
             int index = int.Parse(Console.ReadLine());
             int damage = 1;
-
             string[] input = Console.ReadLine().Split(' ');
 
             while (input[0] != "Supernova")
             {
-
                 int timesToMove = int.Parse(input[1]);
-
                 if (input[0] == "left") {
-
                     for (int i = 0; i < timesToMove; i++)
                     {
-
                         index--;
-
-                        if (index < 0) {
-
+                        
+                        if (index < 0) 
+                        {
                             index = list.Count - 1;
                             damage++;
-
                         }
-
                         list[index] -= damage;
-
-                    }
-
-                    
+                    }  
                 }
-
                 if (input[0] == "right") {
 
                     for (int i = 0; i < timesToMove; i++)
