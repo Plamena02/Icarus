6a729e49531dc33bfc1366570c6314d233752346
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-05-01 11:24:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int index = int.Parse(Console.ReadLine());
+            int damage = 1;
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            while (input[0] != "Supernova")
+            {
+
+                int timesToMove = int.Parse(input[1]);
+
+                if (input[0] == "left") {
+
+                    for (int i = 0; i < timesToMove; i++)
+                    {
+
+                        index--;
+
+                        if (index < 0) {
+
+                            index = list.Count - 1;
+                            damage++;
+
+                        }
+
+                        list[index] -= damage;
+
+                    }
+
+                    
+                }
+
+                if (input[0] == "right") {
+
+                    for (int i = 0; i < timesToMove; i++)
+                    {
+
+                        index++;
+
+                        if (index == list.Count)
+                        {
+
+                            index = 0;
+                            damage++;
+
+                        }
+
+                        list[index] -= damage;
+
+                    }
+
+
+                }
+
+                input = Console.ReadLine().Split(' ');
+
+            }
+
+            foreach (var item in list)
+            {
+                Console.Write("{0} ", item);
+            }
+
+        }
+    }
+}
 
